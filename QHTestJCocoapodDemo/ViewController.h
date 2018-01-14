1fd4a564647f6eea2045d47440bafa15976df5d6
anakinChen <qihuichen@sohu-inc.com>
2018-01-15 01:01:36 +0800

    init

---
+++ QHTestJCocoapodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QHTestJCocoapodDemo
+//
+//  Created by Anakin chen on 2018/1/15.
+//  Copyright © 2018年 Anakin Network Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QHTestJCocoapodDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QHTestJCocoapodDemo
+//
+//  Created by Anakin chen on 2018/1/15.
+//  Copyright © 2018年 Anakin Network Technology. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
